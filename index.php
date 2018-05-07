bea3592af8bb973e161ed61021406e92fcae6fb9
Viraj Malaka <mahanama.vm@gmail.com>
2018-05-08 00:42:05 +0530

    code update

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>CSRF Protection</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <script src="public/js/jquery.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+  <!-- navbar start -->
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">CSRF Protection</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <?php
+          if (isset($_COOKIE['session_cookie'])) {
+            echo "<li class='active'><a href='user-profile.php'>Profile</a></li>";
+          }
+        ?>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <?php
+          if (!isset($_COOKIE['session_cookie'])) {
+            echo "<li><a href='user-login.php'><span class='glyphicon glyphicons-log-in'></span> Login </a></li>";
+          }
+        ?><?php
+          if (isset($_COOKIE['session_cookie'])) {
+            echo "<li><a href='user-logout.php'><span class='glyphicon glyphicons-log-out'></span> Logout</a></li>";
+          }
+        ?>
+      </ul>
+    </div>
+  </nav>
+  <!-- navbar end -->
+
+  <!-- body content start -->
+    <h1 align='center'> CSRF Protection by Synchronizer Token Pattern </h1>
+  <!-- body content end -->
+  </body>
+</html>
